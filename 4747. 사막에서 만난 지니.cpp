32c63075b74e704c0cac737a7990354d9b6bf08a
unsung107 <unsung102@naver.com>
2020-02-28 20:33:08 +0900

    20200228

--- 4747. 사막에서 만난 지니.cpp
+++ 4747. 사막에서 만난 지니.cpp
@@ -1,15 +1,55 @@
 #include <stdio.h>
 
-int T, N;
-int values[10005], numOfValue[10005];
+int T, N, average;
+int values[10005];
 int children[3][2005];
-
+bool visited[10005], check;
 void init() {
     for (int i = 0; i < 10005; i++) {
         values[i] = 0;
-        numOfValue[i] = 0;
+        visited[i] = false;
     }
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 2005; j++) {
+            children[i][j] = -1;
+        }
+    }
+    check = false;
 }
+
+void dfs(int c, int idx, int sum, int now) {
+    if (check) return;
+    children[c][now] = idx;
+
+    if (c == 2 && sum == average) {
+        check = true;
+        return ;
+    }
+    if (sum > average) {
+        children[c][now] = -1;
+        return ;
+    }
+    if (sum == average) {
+        for (int x = 0; x < N; x++) {
+            if (visited[x]) continue;
+            visited[x] = true;
+            dfs(c + 1, x, values[x], 0);
+            if (check) break;
+            visited[x] = false;
+        }
+        return ;
+    }
+
+    for (int i = idx + 1; i < N; i++) {
+        if (visited[i]) continue;
+        visited[i] = true;
+        dfs(c, i, sum + values[i], now + 1);
+        if (check) break;
+        children[c][now + 1] = -1;
+        visited[i] = false;
+    }
+
+};
 
 int main() {
 
@@ -23,9 +63,25 @@ int main() {
             scanf("%d", &eachValue);
             totalValue += eachValue;
             values[n] = eachValue;
-            numOfValue[eachValue] += 1;
         }
 
+        average = totalValue / 3;
+        
+        for (int rx = 0; rx < N; rx ++) {
+            visited[rx] = true;
+            dfs(0,rx,values[rx],0);
+            visited[rx] = false;
+            if (check) break;
+        }
+
+        printf("#%d\n", ro);
+        for (int chi = 0; chi < 3; chi++) {
+            int id = 0;
+            while (children[chi][id] != -1) {
+                printf("%d ", values[children[chi][id++]]);
+            }
+            printf("\n");
+        }
     }
 
     return 0;
